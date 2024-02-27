8259bdb0a758d67de21b9d45e962998ae18aeec9
yashvant <yaswanth.k23@gmail.com>
2024-02-27 17:38:12 +0530

    added rate limiter

--- src/app.js
+++ src/app.js
@@ -4,6 +4,14 @@ const cors = require("cors");
 const expressHttpContext = require("express-http-context");
 const routes = require("./routes");
 const logger = require("../src/middleware/logger")("app");
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 10,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 const app = express();
 app.set("trust proxy", true);
@@ -16,6 +24,7 @@ app.use(
 app.use(cors());
 app.use(express.json({ limit: "10mb", type: ["text/*", "*/json"] }));
 app.use(express.urlencoded({ limit: "10mb", extended: false }));
+app.use("/api", limiter);
 routes(app);
 
 app.use(logger.requestLogger);
